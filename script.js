8b405d1160ef3c0d95fc9dcd14857235ba69c0ce
Greg <example@gmail.com>
2021-12-17 14:50:34 +0300

    cycls

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if (4 == 9) {
 }
 const num = 50;
 
-if(num < 49){
+if (num < 49) {
     console.log("error")
 } else if (num > 100) {
     console.log("error")
@@ -15,13 +15,13 @@ if(num < 49){
     console.log("ok!")
 }
 
-(num == 50)? console.log("ok") : console.log("error");
+(num == 50) ? console.log("ok"): console.log("error");
 
 
 const lol = '19';
 
 
-switch(lol) {
+switch (lol) {
     case '20':
         console.log("error")
         break;
@@ -29,5 +29,20 @@ switch(lol) {
         console.log('ok')
         break;
     default:
-        console.log( "not eat");
+        console.log("not eat");
 }
+
+
+let i = 3;
+
+for (let i = 3; i > 10; i++) {
+   console.log(i)
+}
+
+
+
+
+
+const test = "test text ";
+
+console.log(test.toUpperCase())
